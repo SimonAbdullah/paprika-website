f11f16764cb3eb849e33c7d0fd28fab524302f7f
Ragheed Aloudeh <ragheedaloudeh@gmail.com>
2021-11-01 19:01:09 +0200

    Fixes in home page

--- src/features/home/view/second/home-card-restaurant.components.tsx
+++ src/features/home/view/second/home-card-restaurant.components.tsx
@@ -4,9 +4,10 @@ import Image from "next/image";
 import classes from "./style.module.css";
 import { RestaurantSummaryDto } from "../../../restaurants/services/places/models/restaurant-summary-dto.models";
 import useTranslation from "next-translate/useTranslation";
-import { TranslationFiles } from "../../../../core/core";
+import { PagesUrls, TranslationFiles } from "../../../../core/core";
 import Text from "antd/lib/typography/Text";
 import { LocationIcon } from "../../../shared/icons/icons.components";
+import Link from "next/link";
 
 interface HomeCardRestaurantProps {
   restaurant?: RestaurantSummaryDto;
@@ -16,55 +17,66 @@ const HomeCardRestaurant: FunctionComponent<HomeCardRestaurantProps> = ({
   restaurant,
 }) => {
   const { t } = useTranslation(TranslationFiles.HOME);
+
   return (
-    <Card
-      className={classes.card}
-      cover={
-        <div className={classes.coverContainer}>
-          <Image
-            src={restaurant?.logoImage || "/images/home/first-background.png"}
-            alt={restaurant?.name}
-            layout="fill"
-            objectFit="cover"
-            objectPosition="center"
-          />
-          <div className={classes.content}>
-            <Row>
-              <Col span={24}>
-                <Space direction="vertical" size="small">
-                  <Text className={classes.cardTitle}>{restaurant?.name}</Text>
-                  <div>
-                    <Rate
-                      className={classes.rating}
-                      value={restaurant?.restaurantRate}
-                      allowHalf
-                      disabled
+    <Link href={`${PagesUrls.RESTAURANTS}/${restaurant?.id}`}>
+      <a>
+        <Card
+          hoverable
+          className={classes.card}
+          cover={
+            <div className={classes.coverContainer}>
+              <Image
+                className={classes.image}
+                src={
+                  restaurant?.logoImage || "/images/home/first-background.png"
+                }
+                alt={restaurant?.name}
+                layout="fill"
+                objectFit="cover"
+                objectPosition="center"
+              />
+              <div className={classes.content}>
+                <Row>
+                  <Col span={24}>
+                    <Space direction="vertical" size="small">
+                      <Text className={classes.cardTitle}>
+                        {restaurant?.name}
+                      </Text>
+                      <div>
+                        <Rate
+                          className={classes.rating}
+                          value={restaurant?.restaurantRate}
+                          allowHalf
+                          disabled
+                        />
+                        <Text className={classes.reviews}>{`${
+                          restaurant?.restaurantRate
+                        } ${t("second.reviews")}`}</Text>
+                      </div>
+                      <Text className={classes.address}>
+                        <LocationIcon />
+                        {restaurant?.address}
+                      </Text>
+                    </Space>
+                  </Col>
+                  <div className={classes.plateIcon}>
+                    <Image
+                      src={"/images/home/second-plate.svg"}
+                      alt={t("second.alt.plateImage")}
+                      width="46px"
+                      height="46px"
+                      objectFit="contain"
+                      objectPosition="center"
                     />
-                    <Text className={classes.reviews}>{`${
-                      restaurant?.restaurantRate
-                    } ${t("second.reviews")}`}</Text>
                   </div>
-                  <Text className={classes.address}>
-                    <LocationIcon />
-                    {restaurant?.address}
-                  </Text>
-                </Space>
-              </Col>
-              <div className={classes.plateIcon}>
-                <Image
-                  src={"/images/home/second-plate.svg"}
-                  alt={t("second.alt.plateImage")}
-                  width="46px"
-                  height="46px"
-                  objectFit="contain"
-                  objectPosition="center"
-                />
+                </Row>
               </div>
-            </Row>
-          </div>
-        </div>
-      }
-    />
+            </div>
+          }
+        />
+      </a>
+    </Link>
   );
 };
 
