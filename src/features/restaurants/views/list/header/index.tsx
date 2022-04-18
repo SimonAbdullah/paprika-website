d70cd2069f30036caf45cf131e260a36a27c1070
Ragheed Aloudeh <ragheedaloudeh@gmail.com>
2022-04-18 10:26:47 +0300

    Fix build errors

--- src/features/restaurants/views/list/header/index.tsx
+++ src/features/restaurants/views/list/header/index.tsx
@@ -20,99 +20,94 @@ import { useRouter } from "next/dist/client/router";
 
 interface RestaurantsListHeaderProps {}
 
-const RestaurantsListHeader: FunctionComponent<RestaurantsListHeaderProps> =
-  () => {
-    const { t } = useTranslation(TranslationFiles.RESTAURANTS);
+const RestaurantsListHeader: FunctionComponent<
+  RestaurantsListHeaderProps
+> = () => {
+  const { t } = useTranslation(TranslationFiles.RESTAURANTS);
 
-    const { locale } = useRouter();
+  const { locale } = useRouter();
 
-    const { direction } = useContext(AppContext);
+  const { direction } = useContext(AppContext);
 
-    const { sm, md, lg } = useBreakpoint();
+  const { sm, md, lg } = useBreakpoint();
 
-    const { isGridView, setIsGridView } = useContext(RestaurantsListContext);
+  const { isGridView, setIsGridView } = useContext(RestaurantsListContext);
 
-    const { data } = useInfinityPlaces();
+  const { data } = useInfinityPlaces();
 
-    const [visible, setVisible] = useState(false);
+  const [visible, setVisible] = useState(false);
 
-    const filterButton = (
-      <Button
-        size="small"
-        type="primary"
-        onClick={() => setVisible(true)}
-        icon={<FilterOutlined />}
-      >
-        {t("filter")}
-      </Button>
-    );
+  const filterButton = (
+    <Button
+      size="small"
+      type="primary"
+      onClick={() => setVisible(true)}
+      icon={<FilterOutlined />}
+    >
+      {t("filter")}
+    </Button>
+  );
 
-    return (
-      <div
-        style={{ paddingBottom: "1rem", margin: 0, backgroundColor: "#ffffff" }}
-      >
-        {!lg && (
-          <Drawer
-            placement={direction === "ltr" ? "right" : "left"}
-            visible={visible}
-            onClose={() => setVisible(false)}
-            width={!md ? (sm ? "50%" : "75%") : "33%"}
-          >
-            <RestaurantsFilter />
-          </Drawer>
-        )}
-        <Row justify="space-between">
-          <div style={{ padding: "1rem" }}>
-            <Breadcrumb>
-              <Breadcrumb.Item key={PagesUrls.HOME}>
-                <Link href={PagesUrls.HOME} locale={locale}>
-                  <a style={{ color: "var(--primary-color)" }}>
-                    {t("homePage")}
-                  </a>
-                </Link>
-              </Breadcrumb.Item>
-              <Breadcrumb.Item key={PagesUrls.RESTAURANTS}>
-                {t("restaurants")}
-              </Breadcrumb.Item>
-            </Breadcrumb>
-          </div>
-          {!sm && <div style={{ padding: "1rem" }}>{filterButton}</div>}
-          <div style={{ padding: "1rem" }}>
-            <Space>
-              {sm && !lg && filterButton}
-              <Row
-                align="middle"
-                className={classes.switchViewButton}
-                onClick={() => setIsGridView(true)}
-              >
-                <GridViewIcon type={isGridView ? "active" : "inactive"} />
-                <Text
-                  className={isGridView ? classes.active : classes.inactive}
-                >
-                  {t("gridView")}
-                </Text>
-              </Row>
-              <Row
-                align="middle"
-                className={classes.switchViewButton}
-                onClick={() => setIsGridView(false)}
-              >
-                <ListViewIcon type={isGridView ? "inactive" : "active"} />
-                <Text
-                  className={isGridView ? classes.inactive : classes.active}
-                >
-                  {t("listView")}
-                </Text>
-              </Row>
-              <ItemsCount
-                count={data?.pages?.[0].totalCount}
-                text={t("restaurants")}
-              />
-            </Space>
-          </div>
-        </Row>
-      </div>
-    );
-  };
+  return (
+    <div
+      style={{ paddingBottom: "1rem", margin: 0, backgroundColor: "#ffffff" }}
+    >
+      {!lg && (
+        <Drawer
+          placement={direction === "ltr" ? "right" : "left"}
+          visible={visible}
+          onClose={() => setVisible(false)}
+          width={!md ? (sm ? "50%" : "75%") : "33%"}
+        >
+          <RestaurantsFilter />
+        </Drawer>
+      )}
+      <Row justify="space-between">
+        <div style={{ padding: "1rem" }}>
+          <Breadcrumb>
+            <Breadcrumb.Item key={PagesUrls.HOME}>
+              <Link href={PagesUrls.HOME} locale={locale}>
+                <a style={{ color: "var(--primary-color)" }}>{t("homePage")}</a>
+              </Link>
+            </Breadcrumb.Item>
+            <Breadcrumb.Item key={PagesUrls.RESTAURANTS}>
+              {t("restaurants")}
+            </Breadcrumb.Item>
+          </Breadcrumb>
+        </div>
+        {!sm && <div style={{ padding: "1rem" }}>{filterButton}</div>}
+        <div style={{ padding: "1rem" }}>
+          <Space>
+            {sm && !lg && filterButton}
+            <Row
+              align="middle"
+              className={classes.switchViewButton}
+              onClick={() => setIsGridView(true)}
+            >
+              <GridViewIcon type={isGridView ? "active" : "inactive"} />
+              <Text className={isGridView ? classes.active : classes.inactive}>
+                {t("gridView")}
+              </Text>
+            </Row>
+            <Row
+              align="middle"
+              className={classes.switchViewButton}
+              onClick={() => setIsGridView(false)}
+            >
+              <ListViewIcon type={isGridView ? "inactive" : "active"} />
+              <Text className={isGridView ? classes.inactive : classes.active}>
+                {t("listView")}
+              </Text>
+            </Row>
+            <ItemsCount
+              count={data?.pages?.[0].hits.total.value}
+              text={t("restaurants")}
+            />
+          </Space>
+        </div>
+      </Row>
+    </div>
+  );
+};
 
 export default RestaurantsListHeader;
--- src/features/restaurants/views/list/item/restaurant-list-item.components.tsx
+++ src/features/restaurants/views/list/item/restaurant-list-item.components.tsx
@@ -1,6 +1,5 @@
 import { FunctionComponent } from "react";
 import { Avatar, List, Rate } from "antd";
-import { RestaurantSummaryDto } from "../../../services/places/models/restaurant-summary-dto.models";
 import Text from "antd/lib/typography/Text";
 import classes from "./style.module.css";
 import useTranslation from "next-translate/useTranslation";
@@ -8,9 +7,10 @@ import { PagesUrls, TranslationFiles } from "../../../../../core/core";
 import { LocationBlackIcon } from "../../../../shared/icons/icons.components";
 import Link from "next/link";
 import { useRouter } from "next/dist/client/router";
+import { SearchResultsRestaurants } from "../../../services/restaurants/models/search-results-restaurants.models";
 
 interface RestaurantListItemProps {
-  restaurant: RestaurantSummaryDto;
+  restaurant: SearchResultsRestaurants;
 }
 
 const RestaurantListItem: FunctionComponent<RestaurantListItemProps> = ({
@@ -25,15 +25,15 @@ const RestaurantListItem: FunctionComponent<RestaurantListItemProps> = ({
       <List.Item.Meta
         avatar={
           <Link
-            href={`${PagesUrls.RESTAURANTS}/${restaurant?.name}`}
+            href={`${PagesUrls.RESTAURANTS}/${restaurant?._source.name}`}
             locale={locale}
           >
             <a>
               <Avatar
                 className={classes.avatar}
                 size="large"
-                src={restaurant.logoImage}
-                alt={restaurant?.name}
+                src={restaurant._source.logoimage}
+                alt={restaurant?._source.name}
               />
             </a>
           </Link>
@@ -41,9 +41,9 @@ const RestaurantListItem: FunctionComponent<RestaurantListItemProps> = ({
         title={
           <Text
             className={classes.title}
-            ellipsis={{ tooltip: restaurant?.name }}
+            ellipsis={{ tooltip: restaurant?._source.name }}
           >
-            {restaurant?.name}
+            {restaurant?._source.name}
           </Text>
         }
         description={
@@ -52,21 +52,21 @@ const RestaurantListItem: FunctionComponent<RestaurantListItemProps> = ({
               <LocationBlackIcon />
               <Text
                 ellipsis={{
-                  tooltip: restaurant?.address,
+                  tooltip: restaurant?._source.address,
                 }}
               >
-                {restaurant?.address}
+                {restaurant?._source.address}
               </Text>
             </div>
             <Text>
               <Rate
                 className={classes.rating}
-                value={restaurant?.restaurantRate}
+                value={restaurant?._source.restaurant_rate}
                 allowHalf
                 disabled
               />
               <Text className={classes.reviews}>
-                {t("ratedBy")} {restaurant?.totalRestaurantRatersCount}
+                {t("ratedBy")} {restaurant?._source.rated_by}
               </Text>
             </Text>
           </>
